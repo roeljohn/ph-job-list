6f5a278ed2e25311169595f1fe2bf3d4b86068fa
roeljohn <bobisroeljohn@gmail.com>
2022-07-25 20:30:13 +0800

    register new manila post type and link category and tags to manila post type, register new employee types in all post type

---
+++ functions.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * The functions.php file behaves like a WordPress plugin, adding features and functionality to a 
+ * WordPress site. You can use it to call WordPress functions and to define your own functions.
+ */
+
+/*
+* Creating a function to create our CPT
+*/
+  
+function custom_post_type() {
+  
+    // Set UI labels for Custom Post Type
+        $labels = array(
+            'name'                => _x( 'Manila', 'Post Type General Name', 'ph-job-list' ),
+            'singular_name'       => _x( 'Manila', 'Post Type Singular Name', 'ph-job-list' ),
+            'menu_name'           => __( 'Manila', 'ph-job-list' ),
+            'parent_item_colon'   => __( 'Parent Manila', 'ph-job-list' ),
+            'all_items'           => __( 'All Manila', 'ph-job-list' ),
+            'view_item'           => __( 'View Manila', 'ph-job-list' ),
+            'add_new_item'        => __( 'Add New Manila', 'ph-job-list' ),
+            'add_new'             => __( 'Add New', 'ph-job-list' ),
+            'edit_item'           => __( 'Edit Manila', 'ph-job-list' ),
+            'update_item'         => __( 'Update Manila', 'ph-job-list' ),
+            'search_items'        => __( 'Search Manila', 'ph-job-list' ),
+            'not_found'           => __( 'Not Found', 'ph-job-list' ),
+            'not_found_in_trash'  => __( 'Not found in Trash', 'ph-job-list' ),
+        );
+          
+    // Set other options for Custom Post Type
+          
+        $args = array(
+            'label'               => __( 'manila', 'ph-job-list' ),
+            'description'         => __( 'Metro Manila Jobs', 'ph-job-list' ),
+            'labels'              => $labels,
+            // Features this CPT supports in Post Editor
+            'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+            // You can associate this CPT with a taxonomy or custom taxonomy. 
+            'taxonomies' => array('post_tag','category'),
+            /* A hierarchical CPT is like Pages and can have
+            * Parent and child items. A non-hierarchical CPT
+            * is like Posts.
+            */
+            'hierarchical'        => false,
+            'public'              => true,
+            'show_ui'             => true,
+            'show_in_menu'        => true,
+            'show_in_nav_menus'   => true,
+            'show_in_admin_bar'   => true,
+            'menu_position'       => 5,
+            'can_export'          => true,
+            'has_archive'         => true,
+            'exclude_from_search' => false,
+            'publicly_queryable'  => true,
+            'capability_type'     => 'post',
+            'show_in_rest' => true,
+      
+        );
+          
+        // Registering your Custom Post Type
+        register_post_type( 'manila', $args );
+      
+    }
+      
+    /* Hook into the 'init' action so that the function
+    * Containing our post type registration is not 
+    * unnecessarily executed. 
+    */
+      
+    add_action( 'init', 'custom_post_type', 0 );
+
+    add_action( 'init', 'create_subjects_hierarchical_taxonomy', 0 );
+ 
+//create a custom taxonomy name it subjects for your posts
+ 
+function create_subjects_hierarchical_taxonomy() {
+ 
+// Add new taxonomy, make it hierarchical like categories
+//first do the translations part for GUI
+ 
+  $labels = array(
+    'name' => _x( 'Employee Types', 'taxonomy general name' ),
+    'singular_name' => _x( 'Employee Type', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search Employee Type' ),
+    'all_items' => __( 'All Employee Type' ),
+    'parent_item' => __( 'Parent Employee Type' ),
+    'parent_item_colon' => __( 'Parent Employee Type:' ),
+    'edit_item' => __( 'Edit Employee Type' ), 
+    'update_item' => __( 'Update Employee Type' ),
+    'add_new_item' => __( 'Add New Employee Type' ),
+    'new_item_name' => __( 'New Employee Type Name' ),
+    'menu_name' => __( 'Employee Types' ),
+  );    
+ 
+// Now register the taxonomy
+  register_taxonomy('employee_types',array('post', 'manila'), array(
+    'hierarchical' => true,
+    'labels' => $labels,
+    'show_ui' => true,
+    'show_in_rest' => true,
+    'show_admin_column' => true,
+    'query_var' => true,
+    'rewrite' => array( 'slug' => 'employee_type' ),
+  ));
+ 
+}
