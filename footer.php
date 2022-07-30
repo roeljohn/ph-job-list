b19499cfc64557f98617c23edde4ad95b088559c
roeljohn <bobisroeljohn@gmail.com>
2022-07-30 20:32:28 +0800

    add basic bootstrap template

--- footer.php
+++ footer.php
@@ -7,6 +7,31 @@
  */
 
 ?>
+    <hr class="col-3 col-md-2 mb-5">
+
+<div class="row g-5">
+  <div class="col-md-6">
+    <h2>Starter projects</h2>
+    <p>Ready to beyond the starter template? Check out these open source projects that you can quickly duplicate to a new GitHub repository.</p>
+    <ul class="icon-list ps-0">
+      <li class="d-flex align-items-start mb-1"><a href="https://github.com/twbs/bootstrap-npm-starter" rel="noopener" target="_blank">Bootstrap npm starter</a></li>
+      <li class="text-muted d-flex align-items-start mb-1">Bootstrap Parcel starter (coming soon!)</li>
+    </ul>
+  </div>
+
+  <div class="col-md-6">
+    <h2>Guides</h2>
+    <p>Read more detailed instructions and documentation on using or contributing to Bootstrap.</p>
+    <ul class="icon-list ps-0">
+      <li class="d-flex align-items-start mb-1"><a href="../getting-started/introduction/">Bootstrap quick start guide</a></li>
+      <li class="d-flex align-items-start mb-1"><a href="../getting-started/webpack/">Bootstrap Webpack guide</a></li>
+      <li class="d-flex align-items-start mb-1"><a href="../getting-started/parcel/">Bootstrap Parcel guide</a></li>
+      <li class="d-flex align-items-start mb-1"><a href="../getting-started/vite/">Bootstrap Vite guide</a></li>
+      <li class="d-flex align-items-start mb-1"><a href="../getting-started/contribute/">Contributing to Bootstrap</a></li>
+    </ul>
+  </div>
+</div>
+</main>
   <footer class="pt-5 my-5 text-muted border-top">
     Created by the Bootstrap team &middot; &copy; 2022
   </footer>
