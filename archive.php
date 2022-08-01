452d6576fc66c92a3d00d7c403456ff5e92b971f
roeljohn <bobisroeljohn@gmail.com>
2022-08-01 11:33:00 +0800

    add archive bootstrap template

--- archive.php
+++ archive.php
@@ -15,20 +15,21 @@ get_header();
 
 if ( have_posts() ) { ?>
 	<?php
-		the_archive_title( '<h1 class="page-title">This is archive.php ', '</h1>' );
+		the_archive_title( '<h1 class="page-title">', '</h1>' );
 	?>
 
 	<?php
-		while ( have_posts() ) {
-			the_post();
+		get_template_part( 'template-parts/content/content-archive');
+		//while ( have_posts() ) {
+			//the_post();
 
 			/*
 			* Include the Post-Format-specific template for the content.
 			* If you want to override this in a child theme, then include a file
 			* called content-___.php (where ___ is the Post Format name) and that will be used instead.
 			*/
-			get_template_part( 'template-parts/content/content-excerpt', get_post_format() );
-		}
+			//get_template_part( 'template-parts/content/content-excerpt', get_post_format() );
+		//}
 
 		// Previous/next page navigation.
 } else {
---
+++ template-parts/content/content-archive.php
@@ -0,0 +1,68 @@
+<div class="row g-5">
+    <div class="col-md-8">
+
+<div class="list-group mb-3">
+  <a href="#" class="list-group-item list-group-item-action border-0 px-0" aria-current="true">
+    <div class="d-flex w-100 justify-content-between">
+      <p class="mb-1">List group item heading</p>
+      <small>3 days ago</small>
+    </div>
+  </a>
+  <a href="#" class="list-group-item list-group-item-action border-0 px-0">
+    <div class="d-flex w-100 justify-content-between">
+        <p class="mb-1">List group item heading</p>
+      <small class="text-muted">3 days ago</small>
+    </div>
+  </a>
+  <a href="#" class="list-group-item list-group-item-action border-0 px-0">
+    <div class="d-flex w-100 justify-content-between">
+        <p class="mb-1">List group item heading</p>
+      <small class="text-muted">3 days ago</small>
+    </div>
+  </a>
+</div>
+     
+
+
+      <div class="btn-toolbar justify-content-between" role="toolbar" aria-label="Toolbar with button groups">
+        <div class="btn-group" role="group" aria-label="First group">
+            <button type="button" class="btn btn-outline-secondary">1</button>
+            <button type="button" class="btn btn-outline-secondary">2</button>
+            <button type="button" class="btn btn-outline-secondary">3</button>
+            <button type="button" class="btn btn-outline-secondary">4</button>
+        </div>
+        </div>
+
+    </div>
+
+    <div class="col-md-4">
+      <div class="position-sticky" style="top: 2rem;">
+        <div class="p-4">
+          <h4>Archives</h4>
+          <ol class="list-unstyled mb-0">
+            <li><a href="#">March 2021</a></li>
+            <li><a href="#">February 2021</a></li>
+            <li><a href="#">January 2021</a></li>
+            <li><a href="#">December 2020</a></li>
+            <li><a href="#">November 2020</a></li>
+            <li><a href="#">October 2020</a></li>
+            <li><a href="#">September 2020</a></li>
+            <li><a href="#">August 2020</a></li>
+            <li><a href="#">July 2020</a></li>
+            <li><a href="#">June 2020</a></li>
+            <li><a href="#">May 2020</a></li>
+            <li><a href="#">April 2020</a></li>
+          </ol>
+        </div>
+
+        <div class="p-4">
+          <h4>Elsewhere</h4>
+          <ol class="list-unstyled">
+            <li><a href="#">GitHub</a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Facebook</a></li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </div>
