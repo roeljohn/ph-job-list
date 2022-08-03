595328501654ab682efe15d1a388ca3e80f9afb2
roeljohn <bobisroeljohn@gmail.com>
2022-08-03 20:21:01 +0800

    add nav menu function in header

--- functions.php
+++ functions.php
@@ -3,6 +3,26 @@
  * The functions.php file behaves like a WordPress plugin, adding features and functionality to a 
  * WordPress site. You can use it to call WordPress functions and to define your own functions.
  */
+add_action( 'after_setup_theme', 'register_my_menu' );
+
+function register_my_menu() {
+   register_nav_menu( 'primary', __( 'Primary Menu', 'wp-wedding-theme-one' ) );
+   register_nav_menu( 'footer', __( 'Footer Menu', 'wp-wedding-theme-one' ) );
+}
+function add_menu_link_class( $atts, $item, $args ) {
+  if (property_exists($args, 'link_class')) {
+    $atts['class'] = $args->link_class;
+  }
+  return $atts;
+}
+add_filter( 'nav_menu_link_attributes', 'add_menu_link_class', 1, 3 );
+function add_menu_list_item_class($classes, $item, $args) {
+  if (property_exists($args, 'list_item_class')) {
+      $classes[] = $args->list_item_class;
+  }
+  return $classes;
+}
+add_filter('nav_menu_css_class', 'add_menu_list_item_class', 1, 3);
 /**
  * Enqueue scripts and styles
  */
