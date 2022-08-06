061f25ede4078352ff22532ac72bc166d520766f
roeljohn <bobisroeljohn@gmail.com>
2022-08-06 18:34:51 +0800

    IDK

--- functions.php
+++ functions.php
@@ -23,6 +23,16 @@ function add_menu_list_item_class($classes, $item, $args) {
   return $classes;
 }
 add_filter('nav_menu_css_class', 'add_menu_list_item_class', 1, 3);
+
+
+
+function special_nav_class ($classes, $item) {
+  if (in_array('current-menu-item', $classes) ){
+    $classes[] = 'active ';
+  }
+  return $classes;
+}
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
 /**
  * Enqueue scripts and styles
  */
