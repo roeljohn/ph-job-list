b3728a6b8cb2a2ae6531fb8ef993a1038b94bbcb
roeljohn <bobisroeljohn@gmail.com>
2022-09-07 09:10:59 +0800

    remove for now the form sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,10 +1,6 @@
 <div class="col-md-4">
       <div class="position-sticky" style="top: 2rem;">
-      <?php if (is_single()) : ?>
-        <?php get_template_part( 'template-parts/sidebar/sidebar', 'single' ); ?>
-      <?php else : ?>
-        <?php get_template_part( 'template-parts/sidebar/sidebar', 'form' ); ?>
-      <?php endif; ?>
+      <?php get_template_part( 'template-parts/sidebar/sidebar', 'single' ); ?>
 
 <div class="p-4">
       </div>
